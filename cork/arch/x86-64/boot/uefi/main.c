094f7e8e404ba95a3150322eb1559d0dad818d7a
connor-lawrence <connorlawrence3141@gmail.com>
2026-06-02 19:57:33 -0700

    CUB v1.0 finished

--- cork/arch/x86-64/boot/uefi/main.c
+++ cork/arch/x86-64/boot/uefi/main.c
@@ -1,68 +1,73 @@
 #include <efi.h>
 #include <efilib.h>
 
+#define KERNEL_FILE L"kernel.bin"
+
 // CUB v1.0
 
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    InitializeLib(ImageHandle, SystemTable);
-    Print(L"[CorK UEFI Bootloader] efilib library initalized...\n");
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *uefi_services) {
 
-    EFI_FILE_HANDLE root;
-    EFI_FILE_HANDLE file;
-    EFI_STATUS status;
-    Print(L"[CUB] Variables initialized...\n");
+    InitializeLib(image_handle, uefi_services); // Initializes library
+    Print(L"[CUB] CUB initialized, efilib library initalized...\n");
 
-    EFI_LOADED_IMAGE *loaded_image = NULL;
-    Print(L"[CUB] loaded_image pointer initialized and set to NULL...\n");
+    EFI_STATUS result;
 
-    status = uefi_call_wrapper(BS->HandleProtocol, 3, ImageHandle, &gEfiLoadedImageProtocolGuid, (void**)&loaded_image);
-    Print(L"[CUB] loaded_image set...\n");
-
-    root = LibOpenRoot(loaded_image->DeviceHandle);
-    Print(L"[CUB] Root folder opened...\n");
-
-    status = uefi_call_wrapper(root->Open, 5, root, &file, L"kernel.bin", EFI_FILE_MODE_READ, 0);
-    Print(L"[CUB] kernel.bin opened in read-only...\n");
-
-    EFI_FILE_INFO *info;
-    UINTN info_size = sizeof(EFI_FILE_INFO) + 200;
-    Print(L"[CUB] info_size initialized with info pointer and buffer (200)...\n");
-
-    status = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, info_size, (void**)&info);
-    if (EFI_ERROR(status)) return status;
-    Print(L"[CUB] Memory allocated...\n");
-
-    status = uefi_call_wrapper(file->GetInfo, 4, file, &gEfiFileInfoGuid, &info_size, info);
-    if (EFI_ERROR(status)) return status;
-    Print(L"[CUB] Got file info...\n");
-
-    UINTN size = info->FileSize;
-    Print(L"[CUB] Stored size of kernel.bin...\n");
-
+    EFI_LOADED_IMAGE *boot_info = NULL;
+    EFI_FILE_HANDLE root_directory;
+    EFI_FILE_HANDLE kernel_file;
+    UINTN kernel_info_buffer_size;
+    EFI_FILE_INFO *kernel_info = NULL;
     void *kernel_buffer = NULL;
-    Print(L"[CUB] Initializes pointer for kernel buffer...\n");
+    UINTN kernel_size;
 
-    status = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderCode, size, &kernel_buffer);
-    if (EFI_ERROR(status)) return status;
-    Print(L"[CUB] Kernel buffer allocated...\n");
+    // Get Boot Info
+    result = uefi_call_wrapper(BS->HandleProtocol, 3, image_handle, &gEfiLoadedImageProtocolGuid, (void**)&boot_info);
+    if (EFI_ERROR(result)) {Print(L"[CUB] ! When getting boot info: %r\n", result);} else {Print(L"[CUB] Boot info pulled...\n");}
 
-    status = uefi_call_wrapper(file->Read, 3, file, &size, kernel_buffer);
-    if (EFI_ERROR(status)) return status;
-    Print(L"[CUB] File copied from disk to RAM...\n");
+    // Open Root Directory
+    root_directory = LibOpenRoot(boot_info->DeviceHandle);
+    Print(L"[CUB] Root directory opened...\n");
+    if (!root_directory) {
+        Print(L"[CUB] Failed to open root directory\n");
+        return EFI_DEVICE_ERROR;
+    }
 
-    status = uefi_call_wrapper(file->Close, 1, file);
-    if (EFI_ERROR(status)) return status;
-    Print(L"[CUB] File closed...\n");
+    // Open Kernel File
+    result = uefi_call_wrapper(root_directory->Open, 5, root_directory, &kernel_file, KERNEL_FILE, EFI_FILE_MODE_READ, 0);
+    if (EFI_ERROR(result)) {Print(L"[CUB] ! When opening kernel file: %r\n", result);} else {Print(L"[CUB] Kernel file opened in read-only...\n");}
 
-    status = uefi_call_wrapper(BS->FreePool, 1, info);
-    if (EFI_ERROR(status)) return status;
-    Print(L"[CUB] Allocated RAM freed...\n");
+    // Allocate RAM for Kernel Info
+    kernel_info_buffer_size = sizeof(EFI_FILE_INFO) + 200;
+    result = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, kernel_info_buffer_size, (void**)&kernel_info);
+    if (EFI_ERROR(result)) {Print(L"[CUB] ! When allocating RAM for kernel info: %r\n", result);} else {Print(L"[CUB] RAM allocated for kernel info...\n");}
 
+    // Get Kernel Info
+    result = uefi_call_wrapper(kernel_file->GetInfo, 4, kernel_file, &LoadedImageProtocol, &kernel_info_buffer_size, kernel_info);
+    if (EFI_ERROR(result)) {Print(L"[CUB] ! When getting kernel info: %r\n", result);} else {Print(L"[CUB] Got kernel info...\n");}
+    kernel_size = kernel_info->FileSize;
+
+    // Allocate RAM for Kernel
+    result = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderCode, kernel_size, &kernel_buffer);
+    if (EFI_ERROR(result)) {Print(L"[CUB] ! When allocating RAM for kernel: %r\n", result);} else {Print(L"[CUB] RAM allocated for kernel...\n");}
+
+    // Copy Kernel To RAM
+    result = uefi_call_wrapper(kernel_file->Read, 3, kernel_file, &kernel_size, kernel_buffer);
+    if (EFI_ERROR(result)) {Print(L"[CUB] ! When copying kernel to RAM: %r\n", result);} else {Print(L"[CUB] Kernel copied to RAM...\n");}
+
+    // Close Kernel File
+    result = uefi_call_wrapper(kernel_file->Close, 1, kernel_file);
+    if (EFI_ERROR(result)) {Print(L"[CUB] ! When closing kernel file: %r\n", result);} else {Print(L"[CUB] Kernel file closed...\n");}
+
+    // Free Allocated RAM
+    result = uefi_call_wrapper(BS->FreePool, 1, kernel_buffer);
+    if (EFI_ERROR(result)) {Print(L"[CUB] ! When freeing allocated RAM for kernel buffer: %r\n", result);} else {Print(L"[CUB] Allocated RAM freed for kernel buffer...\n");}
+    result = uefi_call_wrapper(BS->FreePool, 1, kernel_info);
+    if (EFI_ERROR(result)) {Print(L"[CUB] ! When freeing allocated RAM for kernel info: %r\n", result);} else {Print(L"[CUB] Allocated RAM freed for kernel info...\n");}
+
+    // Jump To Kernel
     typedef void (*kernel_entry_t)(void);
     kernel_entry_t kernel_entry = (kernel_entry_t)kernel_buffer;
-    Print(L"[CUB] RAM treated as a function...\n");
-
-    Print(L"[CUB] Jumping to kernel_entry()\n");
+    Print(L"[CUB] Jumping to kernel... \n");
     kernel_entry();
 
     return EFI_SUCCESS;
