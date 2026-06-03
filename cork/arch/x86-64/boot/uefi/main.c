9dfbb2529cfece3a21e71e25eac305c452d8e780
connor-lawrence <connorlawrence3141@gmail.com>
2026-06-03 13:42:15 -0700

    Tweaks CUB Comments

--- cork/arch/x86-64/boot/uefi/main.c
+++ cork/arch/x86-64/boot/uefi/main.c
@@ -22,11 +22,11 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *uefi_servi
     void *kernel_buffer = NULL;
     UINTN kernel_size;
 
-    // Get Boot Info
+    // Get boot info
     result = uefi_call_wrapper(BS->HandleProtocol, 3, image_handle, &gEfiLoadedImageProtocolGuid, (void**)&efi_info);
     if (EFI_ERROR(result)) {Print(L"[CUB] ! When getting boot info: %r\n", result);} else {Print(L"[CUB] Boot info pulled...\n");}
 
-    // Open Root Directory
+    // Open root directory
     root_directory = LibOpenRoot(efi_info->DeviceHandle);
     Print(L"[CUB] Root directory opened...\n");
     if (!root_directory) {
@@ -34,37 +34,37 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *uefi_servi
         return EFI_DEVICE_ERROR;
     }
 
-    // Open Kernel File
+    // Open kernel file
     result = uefi_call_wrapper(root_directory->Open, 5, root_directory, &kernel_file, KERNEL_FILE, EFI_FILE_MODE_READ, 0);
     if (EFI_ERROR(result)) {Print(L"[CUB] ! When opening kernel file: %r\n", result);} else {Print(L"[CUB] Kernel file opened in read-only...\n");}
 
-    // Allocate RAM for Kernel Info
+    // Allocate RAM for kernel info
     kernel_info_buffer_size = sizeof(EFI_FILE_INFO) + 200;
     result = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, kernel_info_buffer_size, (void**)&kernel_info);
     if (EFI_ERROR(result)) {Print(L"[CUB] ! When allocating RAM for kernel info: %r\n", result);} else {Print(L"[CUB] RAM allocated for kernel info...\n");}
 
-    // Get Kernel Info
+    // Get kernel info
     result = uefi_call_wrapper(kernel_file->GetInfo, 4, kernel_file, &gEfiFileInfoGuid, &kernel_info_buffer_size, kernel_info);
     if (EFI_ERROR(result)) {Print(L"[CUB] ! When getting kernel info: %r\n", result);} else {Print(L"[CUB] Got kernel info...\n");}
     kernel_size = kernel_info->FileSize;
 
-    // Allocate RAM for Kernel
+    // Allocate RAM for kernel
     result = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderCode, kernel_size, &kernel_buffer);
     if (EFI_ERROR(result)) {Print(L"[CUB] ! When allocating RAM for kernel: %r\n", result);} else {Print(L"[CUB] RAM allocated for kernel...\n");}
 
-    // Copy Kernel To RAM
+    // Copy kernel to RAM
     result = uefi_call_wrapper(kernel_file->Read, 3, kernel_file, &kernel_size, kernel_buffer);
     if (EFI_ERROR(result)) {Print(L"[CUB] ! When copying kernel to RAM: %r\n", result);} else {Print(L"[CUB] Kernel copied to RAM...\n");}
 
-    // Close Kernel File
+    // Close kernel file
     result = uefi_call_wrapper(kernel_file->Close, 1, kernel_file);
     if (EFI_ERROR(result)) {Print(L"[CUB] ! When closing kernel file: %r\n", result);} else {Print(L"[CUB] Kernel file closed...\n");}
 
-    // Free Allocated RAM
+    // Free allocated RAM
     result = uefi_call_wrapper(BS->FreePool, 1, kernel_info);
     if (EFI_ERROR(result)) {Print(L"[CUB] ! When freeing allocated RAM for kernel info: %r\n", result);} else {Print(L"[CUB] Allocated RAM freed for kernel info...\n");}
 
-    // Passing Screen Info On To Kernel
+    // Sets up screen info to pass to kernel
     EFI_GRAPHICS_OUTPUT_PROTOCOL *gop = NULL;
     result = uefi_call_wrapper(uefi_services->BootServices->LocateProtocol, 3, &gEfiGraphicsOutputProtocolGuid, NULL, (void**)&gop);
     if (EFI_ERROR(result) || !gop || !gop->Mode) {Print(L"[CUB] ! When passing screen info to kernel: %r\n", result);} else {Print(L"[CUB] Passed screen info on to kernel...\n");}
@@ -74,7 +74,7 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *uefi_servi
     boot_info.height = gop->Mode->Info->VerticalResolution;
     boot_info.pixels_per_scanline = gop->Mode->Info->PixelsPerScanLine;
     
-    // Jump To Kernel
+    // Jump to kernel
     typedef void (*kernel_entry_t)(BootInfo*);
     kernel_entry_t kernel_entry = (kernel_entry_t)kernel_buffer;
     Print(L"[CUB] Jumping to kernel... \n");
