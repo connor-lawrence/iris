16cda123ec42d6c956674c5fe679aa2b482167c0
connor-lawrence <connorlawrence3141@gmail.com>
2026-06-03 18:47:18 -0700

    Adds Rainbow Test

--- cork/core/core.c
+++ cork/core/core.c
@@ -12,11 +12,20 @@ void kernel_main(BootInfo* boot_info) {
             framebuffer[y * boot_info->framebuffer.pitch + x] = 0x000000;
         }
     }
-
     for (int x = width / 10; x < width * 9 / 10; x++) {
         for (int y = height / 10; y < height * 9 / 10; y++) {
-            if ((x + y) % 10 == 0) {
-                framebuffer[y * boot_info->framebuffer.pitch + x] = 0xFFFFFF;
+            if ((x + y) % 60 == 0) {
+                framebuffer[y * boot_info->framebuffer.pitch + x] = 0xFF0000;
+            } else if ((x + y + 10) % 60 == 0) {
+                framebuffer[y * boot_info->framebuffer.pitch + x] = 0xFFFF00;
+            } else if ((x + y + 20) % 60 == 0) {
+                framebuffer[y * boot_info->framebuffer.pitch + x] = 0x00FF00;
+            } else if ((x + y + 30) % 60 == 0) {
+                framebuffer[y * boot_info->framebuffer.pitch + x] = 0x00FFFF;
+            } else if ((x + y + 40) % 60 == 0) {
+                framebuffer[y * boot_info->framebuffer.pitch + x] = 0x0000FF;
+            } else if ((x + y + 50) % 60 == 0) {
+                framebuffer[y * boot_info->framebuffer.pitch + x] = 0xFF00FF;
             } else {
                 framebuffer[y * boot_info->framebuffer.pitch + x] = 0x000000;
             }
