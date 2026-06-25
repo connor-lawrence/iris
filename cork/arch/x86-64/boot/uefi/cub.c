1147708692acf1d0b5c7187e86e0dd26b5abf3f2
connor-lawrence <connorlawrence3141@gmail.com>
2026-06-25 13:01:19 -0700

    Fully Handwrites and Imroves CUB, Adds Safety and OCP

--- cork/arch/x86-64/boot/uefi/cub.c
+++ cork/arch/x86-64/boot/uefi/cub.c
@@ -5,20 +5,21 @@
 #include "cub.h"
 
 // CUB v2.0
-
-static EFI_STATUS load_kernel(EFI_HANDLE image_handle, void **kernel_buffer, u64 *kernel_size);
-
 #define KERNEL_FILE L"kernel.elf"
+
+// Function declarations
+static EFI_STATUS load_kernel(EFI_HANDLE image_handle, void **kernel, u64 *kernel_size);
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table) {
 
     EFI_STATUS status;
-
+    
     // Initialize library
     InitializeLib(image_handle, system_table);
-    Print(L"\n[CUB v2] Booting...\n");
+    Print(L"\n[CUB v2] Initialized...\n");
 
-    // Load kernel into RAM
+    // Load kernel into memory
+    Print(L"[CUB v2] Loading kernel into memory...\n");
     void *kernel;
     u64 kernel_size;
     status = load_kernel(image_handle, &kernel, &kernel_size);
@@ -28,89 +29,90 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_tab
         while (1);
     }
 
-    Print(L"[CUB v2] Loaded kernel size: %lu bytes...\n", kernel_size);
-    
-    // Get ELF executable header
+    Print(L"[CUB v2] Loaded kernel size: %lu bytes.\n", kernel_size);
+
+    ////////////////////////////////////////////////////// TEMPORARY
+
+    // Get and print ELF executable header
     Elf64_Executable_Header *e_header = (Elf64_Executable_Header *)kernel;
     Print(L"[CUB v2] ELF magic number: 0x%x %c %c %c, ", e_header->e_ident[0], e_header->e_ident[1], e_header->e_ident[2], e_header->e_ident[3]);
 
     // Check magic number
     if (e_header->e_ident[0] == 0x7F && e_header->e_ident[1] == 'E' && e_header->e_ident[2] == 'L' && e_header->e_ident[3] == 'F') {
-        Print(L"Valid ELF file detected...\n");
-    } else { Print(L"Not a valid ELF file...\n"); }
+        Print(L"Valid ELF file detected.\n");
+    } else { Print(L"Not a valid ELF file.\n"); }
 
+    ////////////////////////////////////////////////////// END OF TEMPORARY
+
+    // Halt
     Print(L"[CUB v2] Finished, halting.\n");
-
-    // Halt so text can be read
     while (1);
     //return EFI_SUCCESS;
 }
 
-static EFI_STATUS load_kernel(EFI_HANDLE image_handle, void **kernel, u64 *kernel_size) {
-    
+static EFI_STATUS load_kernel(EFI_HANDLE image_handle, void **kernel, u64 *kernel_file_size) {
+
     EFI_STATUS status;
+    *kernel = NULL;
 
-    // Fill loaded_image with metadata
-    EFI_LOADED_IMAGE *loaded_image;
-    status = uefi_call_wrapper(BS->HandleProtocol, 3, image_handle, &gEfiLoadedImageProtocolGuid, (void**)&loaded_image);
-    if (EFI_ERROR(status)) return status;
+    EFI_LOADED_IMAGE *loaded_image = NULL;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *filesystem = NULL;
+    EFI_FILE_PROTOCOL *root_directory = NULL;
+    EFI_FILE_PROTOCOL *kernel_file = NULL;
+    u64 kernel_file_info_size = 0;
+    EFI_FILE_INFO *kernel_file_info = NULL;
 
-    // Open root directory
-    EFI_FILE_HANDLE root_directory;
-    root_directory = LibOpenRoot(loaded_image->DeviceHandle);
-    if (!root_directory) {return EFI_DEVICE_ERROR;}
+    // Get loaded image (and device handle)
+    status = uefi_call_wrapper(BS->HandleProtocol, 3, image_handle, &gEfiLoadedImageProtocolGuid, (void **)&loaded_image);
+    if (EFI_ERROR(status)) goto ocp;
 
-    // Open kernel file (gotten from KERNEL_FILE) from root directory
-    EFI_FILE_HANDLE kernel_file;
+    // Get filesystem protocol
+    status = uefi_call_wrapper(BS->HandleProtocol, 3, loaded_image->DeviceHandle, &gEfiSimpleFileSystemProtocolGuid, (void **)&filesystem);
+    if (EFI_ERROR(status)) goto ocp;
+
+    // Open root_directory
+    status = uefi_call_wrapper(filesystem->OpenVolume, 2, filesystem, &root_directory);
+    if (EFI_ERROR(status)) goto ocp;
+
+    // Open kernel file
     status = uefi_call_wrapper(root_directory->Open, 5, root_directory, &kernel_file, KERNEL_FILE, EFI_FILE_MODE_READ, 0);
-    if (EFI_ERROR(status)) return status;
+    if (EFI_ERROR(status)) goto ocp;
 
-    // Find size of kernel info by failing successfully
-    u64 kernel_info_size = 0;
-    status = uefi_call_wrapper(kernel_file->GetInfo, 4, kernel_file, &gEfiFileInfoGuid, &kernel_info_size, NULL);
-    if (EFI_ERROR(status) && status != EFI_BUFFER_TOO_SMALL) return status;
+    // Get size of kernel file info by failing successfully
+    status = uefi_call_wrapper(kernel_file->GetInfo, 4, kernel_file, &gEfiFileInfoGuid, &kernel_file_info_size, NULL);
+    if (EFI_ERROR(status) && status != EFI_BUFFER_TOO_SMALL) goto ocp;
 
-    // Allocate RAM for kernel info
-    EFI_FILE_INFO *kernel_info;
-    status = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, kernel_info_size, (void**)&kernel_info);
-    if (EFI_ERROR(status)) return status;
+    // Allocate memory for kernel file info
+    status = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, kernel_file_info_size, (void **)&kernel_file_info);
+    if (EFI_ERROR(status)) goto ocp;
+    
+    // Get kernel file info
+    status = uefi_call_wrapper(kernel_file->GetInfo, 4, kernel_file, &gEfiFileInfoGuid, &kernel_file_info_size, kernel_file_info);
+    if (EFI_ERROR(status)) goto ocp;
 
-    // Get kernel info
-    status = uefi_call_wrapper(kernel_file->GetInfo, 4, kernel_file, &gEfiFileInfoGuid, &kernel_info_size, kernel_info);
-    if (EFI_ERROR(status)) return status;
+    // Get (and return) kernel file size from kernel file info
+    *kernel_file_size = kernel_file_info->FileSize;
 
-    // Allocate RAM for kernel
-    *kernel_size = kernel_info->FileSize;
-    status = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, *kernel_size, (void**)kernel);
-    if (EFI_ERROR(status)) return status;
+    // Allocate memory for kernel file
+    status = uefi_call_wrapper(BS->AllocatePool, 3, EfiLoaderData, *kernel_file_size, kernel);
+    if (EFI_ERROR(status)) goto ocp;
 
-    // Read kernel into allocated RAM
-    status = uefi_call_wrapper(kernel_file->Read, 3, kernel_file, kernel_size, *kernel);
-    if (EFI_ERROR(status)) return status;
+    // Read kernel file into memory
+    status = uefi_call_wrapper(kernel_file->Read, 3, kernel_file, kernel_file_size, *kernel);
+    if (EFI_ERROR(status)) goto ocp;
 
     // Close kernel file and free allocated RAM from info
     uefi_call_wrapper(kernel_file->Close, 1, kernel_file);
-    uefi_call_wrapper(BS->FreePool, 1, kernel_info);
+    uefi_call_wrapper(root_directory->Close, 1, root_directory);
+    uefi_call_wrapper(BS->FreePool, 1, kernel_file_info);
 
+    return EFI_SUCCESS;
+
+    // OCP
+    ocp:
+    if (*kernel) uefi_call_wrapper(BS->FreePool, 1, *kernel);
+    if (kernel_file_info) uefi_call_wrapper(BS->FreePool, 1, kernel_file_info);
+    if (kernel_file) uefi_call_wrapper(kernel_file->Close, 1, kernel_file);
+    if (root_directory) uefi_call_wrapper(root_directory->Close, 1, root_directory);
     return status;
 }
-
-
-
-/*static EFI_STATUS setup_framebuffer(BootInfo *boot_info) {
-
-    EFI_STATUS status;
-    EFI_GRAPHICS_OUTPUT_PROTOCOL *gop = NULL;
-
-    // Initialize GOP
-    status = uefi_call_wrapper(BS->LocateProtocol, 3, &gEfiGraphicsOutputProtocolGuid, NULL, (void**)&gop);
-    if (EFI_ERROR(status)) return status;
-
-    // Fill boot info with framebuffer info
-    boot_info->framebuffer.address = (u64)gop->Mode->FrameBufferBase;
-    boot_info->framebuffer.width = gop->Mode->Info->HorizontalResolution;
-    boot_info->framebuffer.height = gop->Mode->Info->VerticalResolution;
-    boot_info->framebuffer.pitch = gop->Mode->Info->PixelsPerScanLine;
-    
-    return EFI_SUCCESS;
-}*/
